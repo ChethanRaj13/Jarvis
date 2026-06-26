a3261609bfb6d5e03127be202e529bcfd8da8f7f
preetham hj <hjpreetham1@gmail.com>
2026-06-26 14:54:47 +0530

    correct  folder structure for the frontend

--- WindowsAIAssistant/windowsAssistant.UI/obj/Release/net10.0-windows/windowsAssistant.UI.AssemblyInfo.cs
+++ WindowsAIAssistant/windowsAssistant.UI/obj/Release/net10.0-windows/windowsAssistant.UI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("windowsAssistant.UI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073aa1558276cc9157ecb3425534bd44951668cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("windowsAssistant.UI")]
